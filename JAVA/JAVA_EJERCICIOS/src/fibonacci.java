d6c55de3ac5b988793f5d00fb3919d41b813cb58
JocRivera <jose_mrivera@soy.sena.edu.co>
2024-01-31 16:54:21 -0500

    ejercicio fibonacci

--- JAVA/JAVA_EJERCICIOS/src/fibonacci.java
+++ JAVA/JAVA_EJERCICIOS/src/fibonacci.java
@@ -1,5 +1,12 @@
 public class fibonacci {
     public static void main(String[] args) {
         System.out.println("secuencia fibonacci en JAVA");
+	int n=10, a= 0, b= 1, c;
+	for (int i= 0 ; i<n ; 1++){
+	System.out.println(a);
+	c = a + b;
+	a=b;
+	b=c;
+}
     }
 }
