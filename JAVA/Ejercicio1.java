2f3d1446cbe038e2e0d3bb418702c108f30c0353
Jose Rivera <149016549+JocRivera@users.noreply.github.com>
2024-02-06 09:40:22 -0500

    changes

--- JAVA/Ejercicio1.java
+++ JAVA/Ejercicio1.java
@@ -1,6 +1,6 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
-        System.out.println("Hola, este es mi primer programa en Java en Linux.");
+        System.out.println("Hola, este es mi primer programa en Java.");
     }
 }
 
