84efc6e0d06b899f50204d0da3310fa2376c4199
JocRivera <jose_mrivera@soy.sena.edu.co>
2024-02-09 15:48:52 -0500

    ejercicios

--- JAVA/JAVA_EJERCICIOS/src/Ejercicio5.java
+++ JAVA/JAVA_EJERCICIOS/src/Ejercicio5.java
@@ -1,6 +1,21 @@
 /*Inversión de Vectores: Implementa un programa que invierta el orden de los elementos en un vector */
+import java.util.Scanner;
 public class Ejercicio5 {
+public static int[] invertir(int[]arreglo){
+	int[] resultado = new int[arreglo.lenght];
+	for(int i=0; i< arreglo.lenght; i++){
+		resultado[i]= arreglo[arreglo.lenght -1 -i];
+}
+	return resultado;
+}
     public static void main(String[] args) {
-        
+        int venctor[]= new int[10];
+	Scanner sc= new Scanner(System.in);
+	System.out.println("ingrese un orden numerico de 10 valores: ");
+	for(int i=0; i<=vector.lenght-1; i++){
+		vector[]=sc.nextInt();
+}
+	int []vectorInvertido=invertir(vector);
+	System.out.println("valores invertidos: "+ Arrays.toString(vectorIrInvertido));
     }
 }
